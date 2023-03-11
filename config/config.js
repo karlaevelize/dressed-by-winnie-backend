c06f74927df7a552e478bd2c0b132f4e129af721
karlaevelize <k.evelize@gmail.com>
2023-03-11 19:36:49 +0100

    Database config

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    dialect: "postgres",
+    url: process.env.DATABASE_URL_DEV,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
