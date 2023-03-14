85a5ba076f78841eed27e27248097b95dc0dbb5e
karlaevelize <k.evelize@gmail.com>
2023-03-14 11:29:07 +0100

    Production config

--- config/config.js
+++ config/config.js
@@ -5,18 +5,8 @@ module.exports = {
     dialect: "postgres",
     url: process.env.DATABASE_URL_DEV,
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
   },
 };
